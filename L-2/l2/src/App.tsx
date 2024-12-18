15dd6174b0b9bcd220adb3d03f6e0f0b2dd8828a
Al Ev <alexey_e@mail.ru>
2024-12-18 08:46:30 +0300

    L-2 done!

--- L-2/l2/src/App.tsx
+++ L-2/l2/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Calendar />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload it now.
         </p>
         <a
           className="App-link"
