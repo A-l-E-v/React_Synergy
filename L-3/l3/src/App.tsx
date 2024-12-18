dc2d9164a8aa9e5a166d51b4811e67564aea06fe
Al Ev <alexey_e@mail.ru>
2024-12-18 13:33:24 +0300

    btn&input done - b4 styling

--- L-3/l3/src/App.tsx
+++ L-3/l3/src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState}from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState<string>('');
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+};
+const handleButtonClick = () => {
+  setInputValue(''); // Очищаем поле ввода
+};
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+
+        <div>
+            <input 
+                type="text" 
+                value={inputValue} 
+                onChange={handleInputChange} 
+            />
+            <button onClick={handleButtonClick}>
+                Очистить
+            </button>
+        </div>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
